ffe7ae1196f6cb2d5cc739da1e72dacc603a0eee
raramuri <samarth.garg@zopsmart.com>
2022-02-08 11:22:20 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var clickButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clickButton = findViewById(R.id.clickButton)
+        clickButton.setOnClickListener {
+            Toast.makeText(applicationContext , "It's Samarth ", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+}
